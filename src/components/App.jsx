01e026269101fd05f0c0a22e1f40d3ef2c768a37
shaavan <shaavan.github@gmail.com>
2023-02-05 19:32:15 +0530

    Keeper app: Part 2;
    1. Added ability to dynamically update Notes as they are added to
       notes.js using React props.
    2. Used arr.map() function to render all the notes in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
-
+import notes from "../notes";
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map((note) =>
+                <Note
+                    key={note.id}
+                    title={note.title}
+                    content={note.content}
+                />
+            )};
             <Footer />
         </div>
     )
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-function Note() {
-    const title = "This is the note title";
-    const content = "This is the note content";
-
+function Note(props) {
     return (
         <div class="note">
-            <h1>{title}</h1>
-            <p>{content}</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
